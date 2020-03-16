caaf4c1288a145f768ae284d6cf45c63a09e26ab
danielle <danielle@stackery.io>
2020-03-16 11:42:23 -0700

    Add recursion example

---
+++ recursion/factorial.js
@@ -0,0 +1,17 @@
+function factorial (num) {
+  if (num < 2) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+
+/**
+ * A function that computes a factorial
+ * ex. 4! = 4 * 3 * 2 * 1
+ */
