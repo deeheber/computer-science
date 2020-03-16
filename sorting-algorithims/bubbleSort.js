1e601edfe81854d71016fc410698381e53550847
danielle <danielle@stackery.io>
2020-03-16 13:10:25 -0700

    Bubble sort

---
+++ sorting-algorithims/bubbleSort.js
@@ -0,0 +1,30 @@
+/**
+ * Sort numbers in ascending  order
+ * Outer loop checks if something was swapped
+ * Inner loop compares two nums at a time
+ * ...if  not in order > swap the two numbs
+ *
+ * Inefficient...don't use in prod
+ */
+
+function bubbleSort (nums) {
+  let wasSwapped = false;
+  do {
+    wasSwapped = false;
+    console.log('Debug: ', nums);
+    for (let i = 0; i <= nums.length; i++) {
+      const firstNum = nums[i];
+      const secondNum = nums[i + 1];
+
+      if (secondNum && secondNum < firstNum) {
+        nums[i] = secondNum;
+        nums[i + 1] = firstNum;
+        wasSwapped = true;
+      }
+    }
+  } while (wasSwapped);
+  return nums;
+}
+
+// Test case
+console.log(bubbleSort([10, 5, 3, 8, 2, 6, 4, 7, 9, 1]));
