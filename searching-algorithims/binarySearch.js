0dacd34fc73f06c022f23dc0af52f674690b1ab9
danielle <danielle@stackery.io>
2020-04-16 09:26:07 -0700

    alt version binarySearch

--- searching-algorithims/binarySearch.js
+++ searching-algorithims/binarySearch.js
@@ -24,3 +24,31 @@ function binarySearch (target, nums) {
 // The nums array must be sorted already
 console.log(binarySearch(55, [8, 9, 10, 11, 12, 13])); // false
 console.log(binarySearch(5, [5, 6, 7, 8, 9])); // true
+
+// Alternate version
+// function binarySearch (target, nums) {
+//   let floorIndex = 0;
+//   let ceilingIndex = nums.length - 1;
+
+//   while (floorIndex + 1 < ceilingIndex) {
+//     const middleIndex = floorIndex + Math.floor((ceilingIndex - floorIndex) / 2);
+//     const guessValue = nums[middleIndex];
+
+//     console.log('guess is ', guessValue);
+//     console.log(floorIndex, middleIndex, ceilingIndex);
+//     if (target === nums[0] || target === nums[nums.length - 1] || target === guessValue) {
+//       return true;
+//     }
+
+//     if (target > guessValue) {
+//       // go right
+//       floorIndex = middleIndex;
+//     } else {
+//       // go left
+//       ceilingIndex = middleIndex;
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(binarySearch(7, [6, 7, 8, 9, 10])); // true
