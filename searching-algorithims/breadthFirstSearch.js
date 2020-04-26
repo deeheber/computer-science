0b0db2ac6fb2413316f9e1267cb658784714ab82
danielle <danielle@stackery.io>
2020-04-25 18:44:01 -0700

    breadth first search

---
+++ searching-algorithims/breadthFirstSearch.js
@@ -0,0 +1,29 @@
+/**
+ * 1. Print (visit) node
+ * 2. Enqueue left child and right child
+ * 4. Remove original node from the queue
+ */
+
+function breadthFirstSearch(rootNode) {
+  if (!rootNode) {
+    return;
+  }
+
+  const queue = [];
+  queue.push(rootNode);
+
+  while (queue.length > 0) {
+    const currentNode = queue[0];
+    console.log(`Visiting: ${currentNode}`);
+
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+
+    queue.shift();
+  }
+}
